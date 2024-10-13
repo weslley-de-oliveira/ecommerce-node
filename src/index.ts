c3feade2641d6e62fbaaa59ad5ba27ff7c4ca50a
weslley-de-oliveira <weslley.oli93@gmail.com>
2024-10-13 15:47:14 -0300

    Criando o endpoint post users

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,34 @@ import express, { Request , Response} from "express";
 
 const app = express();
 
+app.use(express.json());
+
+const usuarios = [
+    {
+        nome: "Weslley de Oliveira",
+        idade: 30
+    },
+    {
+        nome: "Maria de Oliveira",
+        idade: 56
+    }
+]
+
 app.get("/", (req: Request, res: Response) => {
     res.send("Bem vindo ao curso de node.js - Manual");
 });
 
 app.get("/users", (req: Request, res: Response) => {
-    const usuarios = [
-        {
-            nome: "Weslley de Oliveira",
-            idade: 30
-        },
-        {
-            nome: "Maria de Oliveira",
-            idade: 56
-        }
-    ]
     res.send(usuarios);
-})
+});
+
+app.post("/users", (req: Request, res: Response) => {
+    let user = req.body;
+    usuarios.push(user);
+    res.send({
+        message: "Usuário criado com sucesso!"
+    });
+});
 
 app.listen(3000, () => {
     console.log("Servidor ativo na porta 3000");
