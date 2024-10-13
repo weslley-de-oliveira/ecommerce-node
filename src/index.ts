2b5f43fdf10d09b97fd9a95f162f1af039496c4e
weslley-de-oliveira <weslley.oli93@gmail.com>
2024-10-13 15:36:11 -0300

    Criando o endpoint users

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-import express from "express";
+import express, { Request , Response} from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
     res.send("Bem vindo ao curso de node.js - Manual");
+});
+
+app.get("/users", (req: Request, res: Response) => {
+    const usuarios = [
+        {
+            nome: "Weslley de Oliveira",
+            idade: 30
+        },
+        {
+            nome: "Maria de Oliveira",
+            idade: 56
+        }
+    ]
+    res.send(usuarios);
 })
 
 app.listen(3000, () => {
