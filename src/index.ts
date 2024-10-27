33ab770c16cd768259837e0b539b535a4781f998
weslley-de-oliveira <weslley.oli93@gmail.com>
2024-10-27 01:41:06 -0300

    Adicionando error de rota não encontrada

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import { initializeApp } from 'firebase-admin/app';
 import { routes } from "./routes/index";
 import { erroHandler } from "./middlewares/error-handler.middleware";
+import { PageNotFoundHandler } from "./middlewares/page-not-found.middleware";
 
 initializeApp();
 const app = express();
 
 routes(app);
+PageNotFoundHandler(app);
 erroHandler(app);
 
 app.listen(3000, () => {
