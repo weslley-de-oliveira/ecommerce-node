c5792b3f32ed036872ac7978731a1f079c15621e
weslley-de-oliveira <weslley.oli93@gmail.com>
2024-10-29 22:08:24 -0300

    Recuperando dados do usuário logado e liberando rota de login

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -1,13 +1,22 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { UnauthorizedError } from '../errors/unauthorized.error';
+import { getAuth } from 'firebase-admin/auth';
 
 export const auth = (app: express.Express) => {
-    app.use((req: Request, res: Response, next: NextFunction) => {
+    app.use(async (req: Request, res: Response, next: NextFunction) => {
+        if (req.method === 'POST' && req.url.endsWith('/auth/login')) {
+            return next();
+        }
+
         const token = req.headers.authorization?.split("Bearer ")[1];
 
         if (token) {
-            console.log(token);
-            return next();
+            try {
+                const decodeIdToken = await getAuth().verifyIdToken(token, true);
+                console.log(decodeIdToken);
+                return next();
+            } catch (error) {
+                next(new UnauthorizedError());            }
         }
 
         next(new UnauthorizedError());
