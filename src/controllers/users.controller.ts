ca8fe2bf4dbc13548eaf136d667ecdd6592056ab
weslley-de-oliveira <weslley.oli93@gmail.com>
2026-07-24 14:34:16 -0300

    Adicionando asyncHandler

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -5,101 +5,81 @@ import { ValidationError } from "../errors/validation.error";
 
 export class UsersController {
   static async getAll(req: Request, res: Response, next: NextFunction) {
-    try {
-      const snapshot = await getFirestore().collection("users").get();
-      const users = snapshot.docs.map((doc) => {
-        return { id: doc.id, ...doc.data() };
-      });
+    const snapshot = await getFirestore().collection("users").get();
+    const users = snapshot.docs.map((doc) => {
+      return { id: doc.id, ...doc.data() };
+    });
 
-      res.send(users);
-    } catch (error) {
-      next(error);
-    }
+    res.send(users);
   }
 
   static async getById(req: Request, res: Response, next: NextFunction) {
-    try {
-      const userId = req.params.id as string;
-      const doc = await getFirestore().collection("users").doc(userId).get();
+    const userId = req.params.id as string;
+    const doc = await getFirestore().collection("users").doc(userId).get();
 
-      if (!doc.exists) {
-        throw new NotFoundError();
-      }
-
-      res.send({ id: doc.id, ...doc.data() });
-    } catch (error) {
-      next(error);
+    if (!doc.exists) {
+      throw new NotFoundError();
     }
+
+    res.send({ id: doc.id, ...doc.data() });
   }
 
   static async create(req: Request, res: Response, next: NextFunction) {
-    try {
-      const user = req.body;
+    const user = req.body;
 
-      if (!user.email || !user.email?.length) {
-        throw new ValidationError("E-mail obrigatório!");
-      }
-
-      if (!user.nome || !user.nome?.length) {
-        throw new ValidationError("Nome obrigatório!");
-      }
-
-      await getFirestore().collection("users").add(user);
-
-      res.status(201).send({
-        message: "Usuário criado com sucesso!",
-      });
-    } catch (error) {
-      next(error);
+    if (!user.email || !user.email?.length) {
+      throw new ValidationError("E-mail obrigatório!");
     }
+
+    if (!user.nome || !user.nome?.length) {
+      throw new ValidationError("Nome obrigatório!");
+    }
+
+    await getFirestore().collection("users").add(user);
+
+    res.status(201).send({
+      message: "Usuário criado com sucesso!",
+    });
   }
 
   static async update(req: Request, res: Response, next: NextFunction) {
-    try {
-      const user = req.body;
-      const userId = req.params.id as string;
-      const docRef = getFirestore().collection("users").doc(userId);
+    const user = req.body;
+    const userId = req.params.id as string;
+    const docRef = getFirestore().collection("users").doc(userId);
 
-      if (!user.email || !user.email?.length) {
-        throw new ValidationError("E-mail obrigatório!");
-      }
-
-      if (!user.nome || !user.nome?.length) {
-        throw new ValidationError("Nome obrigatório!");
-      }
-
-      if (!(await docRef.get()).exists) {
-        throw new NotFoundError();
-      }
-
-      await docRef.set({
-        nome: user.nome,
-        email: user.email,
-      });
-
-      res.send({
-        message: "Usuário alterado  com sucesso!",
-      });
-    } catch (error) {
-      next(error);
+    if (!user.email || !user.email?.length) {
+      throw new ValidationError("E-mail obrigatório!");
     }
+
+    if (!user.nome || !user.nome?.length) {
+      throw new ValidationError("Nome obrigatório!");
+    }
+
+    if (!(await docRef.get()).exists) {
+      throw new NotFoundError();
+    }
+
+    await docRef.set({
+      nome: user.nome,
+      email: user.email,
+    });
+
+    res.send({
+      message: "Usuário alterado  com sucesso!",
+    });
   }
 
   static async delete(req: Request, res: Response, next: NextFunction) {
-    try {
-      const userId = req.params.id as string;
-      const docRef = getFirestore().collection("users").doc(userId);
+    const userId = req.params.id as string;
+    const docRef = getFirestore().collection("users").doc(userId);
 
-      if (!(await docRef.get()).exists) {
-        throw new NotFoundError();
-      }
-      await docRef.delete();
-
-      res.send({
-        message: "Usuário excluído com scesso!",
-      });
-    } catch (error) {
-      next(error);
+    if (!(await docRef.get()).exists) {
+      throw new NotFoundError();
     }
+    await docRef.delete();
+
+    res.send({
+      message: "Usuário excluído com scesso!",
+    });
   }
 }
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import asyncHandler from "express-async-handler";
 import { UsersController } from "../controllers/users.controller";
 
 // Módulo do Express pra criação e configuração de Rotas
 export const userRoutes = express.Router();
 
-userRoutes.get("/users", UsersController.getAll);
-userRoutes.get("/users/:id", UsersController.getById);
-userRoutes.post("/users", UsersController.create);
-userRoutes.put("/users/:id", UsersController.update);
-userRoutes.delete("/users/:id", UsersController.delete);
+userRoutes.get("/users", asyncHandler(UsersController.getAll));
+userRoutes.get("/users/:id", asyncHandler(UsersController.getById));
+userRoutes.post("/users", asyncHandler(UsersController.create));
+userRoutes.put("/users/:id", asyncHandler(UsersController.update));
+userRoutes.delete("/users/:id", asyncHandler(UsersController.delete));
