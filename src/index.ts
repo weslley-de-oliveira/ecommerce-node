f8eb2459f19fdbb48c86042e37cf77b2f09a3d99
weslley-de-oliveira <weslley.oli93@gmail.com>
2026-07-24 09:13:30 -0300

    Instalando e configurando o Firebase Firestore

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import express from "express"
+import express from "express";
+import { initializeApp } from "firebase-admin/app";
 import { routes } from "./routes";
 
+initializeApp();
 const app = express();
 
-routes(app)
+routes(app);
 
 // executa algo quando o subir o servidor
 app.listen(3000, () => {
-    console.log("Servidor ativo na porta 3000");
-})
+  console.log("Servidor ativo na porta 3000");
+});
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 import { userRoutes } from "./users.route";
 
 export const routes = (app: express.Express) => {
-    app.use(express.json());
-    app.use(userRoutes);
-}
+  app.use(express.json());
+  app.use(userRoutes);
+};
