61155242337b51a9207e97e289dec6df54be4769
weslley-de-oliveira <weslley.oli93@gmail.com>
2026-07-24 07:52:14 -0300

    Adicionando serviços de CRUD para os usuários

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,50 @@
-import express from "express"
+import express, { Request, Response } from "express"
 
 const app = express();
 
-// Exemplo de requisição get
-app.get("/", (req, res) => {
+app.use(express.json());
+
+let usuarios: {id: number, nome: string, email: string}[] = []
+let id = 0;
+
+app.get("/", (req: Request, res: Response) => {
     res.send("Bem vindo ao curso de NodeJS")
+})
+
+app.get("/users", (req: Request, res: Response) => {
+    res.send(usuarios)
+})
+
+app.get("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let user = usuarios.find(user => user.id === userId);
+    
+    res.send(user)
+})
+
+app.post("/users", (req: Request, res: Response) => {
+    let user = req.body;
+    user.id = id++
+    usuarios.push(user)
+
+    res.send(usuarios)
+})
+
+app.put("/users/:id", (req: Request, res: Response) => {
+    let user = req.body;
+    let userId = Number(req.params.id);
+    const index = usuarios.findIndex(user => user.id === userId)
+
+    usuarios[index] = {...user, id: userId};
+
+    res.send(usuarios)
+})
+
+app.delete("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    const newUsuarios = usuarios.filter(user => user.id !== userId)
+
+    res.send(newUsuarios)
 })
 
 // executa algo quando o subir o servidor
