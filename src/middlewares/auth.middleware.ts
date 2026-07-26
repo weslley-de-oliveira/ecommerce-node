d6df6453f8a02b164451a30d9dbd59b12d9d0613
weslley-de-oliveira <weslley.oli93@gmail.com>
2026-07-26 10:41:36 -0300

    Liberando rota do login

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,12 +1,25 @@
 import express, { NextFunction, Request, Response } from "express";
 import { UnauthorizedError } from "../errors/unauthorized.erro";
+import { getAuth } from "firebase-admin/auth";
 
 export const auth = (app: express.Express) => {
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization?.split("Bearer")[1];
+  app.use(async (req: Request, res: Response, next: NextFunction) => {
+    if (req.method === "POST" && req.url.startsWith("/auth/login")) {
+      return next();
+    }
+
+    const token = req.headers.authorization?.split("Bearer ")[1];
 
     if (token) {
-      return next();
+      console.log(token);
+      try {
+        const decodeIdToken = await getAuth().verifyIdToken(token, true);
+        console.log(decodeIdToken);
+
+        return next();
+      } catch (error) {
+        next(new UnauthorizedError());
+      }
     }
 
     next(new UnauthorizedError());
